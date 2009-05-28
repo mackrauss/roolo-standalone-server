d33791a6bc2735406bb65450165b217087d2e10d
ccelorrio <ccelorrio@294c3b0c-171f-11de-a6cc-771c510851eb>
2009-05-28 16:55:57 +0000

    Last Smart Moment Minute tweaks

--- trunk/buffer-server/src/uigenerator/AskForSolution.java
+++ trunk/buffer-server/src/uigenerator/AskForSolution.java
@@ -13,7 +13,7 @@ public class AskForSolution implements IUiGenerator {
 		
 		output += 		
 			"<form method='post' enctype='multipart/form-data' id='uploadform'>" +
-			"	<input type='file' name='solution' size='30' class='fileinput'/><a href='#' class='uploadfile' onclick='uploadFile()'>Upload File</a>" +
+			"	<input type='file' name='solution' size='30' class='fileinput'/> <a href='#' class='uploadfile' onclick='clearInteractionPane(); uploadFile();'>Upload File</a>" +
 			"</form>" +
 			"<iframe src='' id='fileframe' name='fileframe' onLoad='sendImage()' style='visibility:hidden'></iframe>";
        
