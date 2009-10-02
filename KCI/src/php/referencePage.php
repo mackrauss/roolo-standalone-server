068c11f403d4eaa4ee35fd616382de84b6786fdf
somebodyiknow <somebodyiknow@294c3b0c-171f-11de-a6cc-771c510851eb>
2009-10-02 21:57:15 +0000

    The References as well as the Reference page should have the right look and feel. We still need to implement the actual addition/updating of a new reference. The majority of that work is in place thought.

--- KCI/src/php/referencePage.php
+++ KCI/src/php/referencePage.php
@@ -1,172 +1,215 @@
 <?php
-require_once 'dataModels/Elo.php';
-require_once 'dataModels/Citation.php';
 require_once 'ReferenceCategories.php';
 require_once 'RooloClient.php';
+require_once 'dataModels/Reference.php';
+
+require_once './header.php';
+
+error_reporting(E_STRICT);
 
 $rooloClient = new RooloClient();
 
+
 if ($_REQUEST['action'] == 'addElo'){
 	
-	$eloTitle = $_REQUEST['eloName'];
-	$eloContent = $_REQUEST['eloContent'];
+	// Add the submitted Reference to Roolo
+	$submittedReference = new Reference();
+	//get the author from the session
+	//$submittedReference->set_author($_SESSION['user']);
+	$submittedReference->set_title($_POST["referenceTitle"]);
+	$submittedReference->set_annotation($_POST["annotationText"]);
+	$submittedReference->set_citation($_POST["citationText"]);
 	
-	$citation = new Citation();
-	$citation->set_title($eloTitle);
+	echo $_POST["referenceCategoryList"];
 	
-	//TODO fill in the author info
-	$citation->set_author('rokham');
-	$citation->setContent($eloContent);
-	//TODO addElo needs to return the URI for the added ELO
-	// We need that to add the appropriate tags for the given citation
-	echo $rooloClient->addElo($citation);		
-	
-	//TODO create tags for this citation
+	$reference = new Reference($submittedReference->generateXml());
 
 	$action = 'update';
+} else if ($_REQUEST['action'] == 'update') {
 
-} elseif ($_REQUEST['action'] == 'update'){
-	
-	$eloTitle = $_REQUEST['eloName'];
-	$eloContent = $_REQUEST['eloContent'];
-	$eloId = $_REQUEST['id'];
-	$eloId = $rooloClient->getUriDomain() . $eloId . '.Citation';
-	
-	//TODO Do we need to escape special characters for our URI 
-//	$eloId = str_replace(':', '\:', $eloId);
-//	$eloId = str_replace('-', '\-', $eloId);
-
-	$citation = new Citation();
-	$citation->set_uri($eloId);
-	$citation->set_title($eloTitle);
-	
-	//TODO fill in the author info
-	$citation->set_author('rokham');
-	$citation->setContent($eloContent);
-	echo $rooloClient->updateElo($citation);
+	// Updte the submitted Reference in Roolo
+	echo 'updated';
+	// need to fill this object with all the send in params of the form
+	$reference = new Reference();
 	
 	$action = 'update';
-} else{
-
+}else{
 	if (isset($_REQUEST['id'])){
-		
+		$id = $_REQUEST['id'];
+		$eloId = $rooloClient->getUriDomain() . $id;
+		$reference = new Reference();
+		$reference = $rooloClient->retrieveElo($eloId);
+
+		// get all the tags for the current reference
+		$tags = $rooloClient->search('type:Tag AND owneruri:'.$reference->get_uri(true), 'metadata');
+		$tagsArray = array();
+		foreach ($tags as $tag){
+			$tagsArray[] = $tag->get_title();
+		}
+		$refernceTagsString = implode(', ', $tagsArray);
+
 		$action = 'update';
-		$eloId = $_REQUEST['id'];
-		
-		$citation = new Citation();
-		$citation = $rooloClient->retrieveElo($eloId);
-		$citation->set_uri($rooloClient->getUriDomain() . $eloId);
-		
-		
-		//TODO Get the tags corresponding to this Citation
-		$citationTagsArray = $rooloClient->search('type:Tag AND owneruri:'. $citation->get_uri(true), 'metadata');
-		$citationTagTitles = array();
-		foreach ($citationTagsArray as $citationTag){
-			$citationTagTitles[] = '"' . $citationTag->get_title() . '"';
- 		}
- 		if (sizeof($citationTagTitles) > 0){
-			$citationTagsString = implode(', ', $citationTagTitles);
- 		}else {
- 			$citationTagsString = '';
- 		}
-		
 	}else {
-		$citation = new Citation();
+		$reference = new Reference();
 		$action = 'addElo';
 	}
 }
 
 ?>
-
-
-<html>
-
-
-<!--<head>-->
-<!---->
-<!-- Load TinyMCE -->
-<script type="text/javascript" src="/library/js/jquery-1.3.2.min.js"></script>
-<script type="text/javascript" src="/tinymce/jscripts/tiny_mce/jquery.tinymce.js"></script>
-
-<script type="text/javascript"><!--
-	$().ready(function() {
-		$('textarea.tinymce').tinymce({
-			// Location of TinyMCE script
-			script_url : '/tinymce/jscripts/tiny_mce/tiny_mce.js',
-
-			// General options
-			theme : "advanced",
-			plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-
-			// Theme options
-			theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-			theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
-			theme_advanced_toolbar_location : "top",
-			theme_advanced_toolbar_align : "left",
-			theme_advanced_statusbar_location : "bottom",
-			theme_advanced_resizing : true,
-
-			// Example content CSS (should be your site CSS)
-			//content_css : "css/content.css",
-
-			// Drop lists for link/image/media/template dialogs
-			template_external_list_url : "lists/template_list.js",
-			external_link_list_url : "lists/link_list.js",
-			external_image_list_url : "lists/image_list.js",
-			media_external_list_url : "lists/media_list.js",
-
-			// Replace values for the template plugin
-			template_replace_values : {
-				username : "Some User",
-				staffid : "991234"
+		<script type='text/javascript' src='../../library/js/jquery-1.3.2.min.js'></script>
+		<script type='text/javascript' src='../../library/js/jquery-ui-1.7.2.custom.min.js'></script>
+		<meta http-equiv='Content-Type' content='text/html; charset=ISO-8859-1'>
+		<title>References Page</title>
+		<style>
+			#titleArea { 
+	 	 		width:80%;
+				padding:10px 0px 20px 0px;
+	 	 		margin-left: auto;
+				margin-right: auto;
 			}
-		});
-	});
-</script>
-</head>
-
-
-
-<body>
-	<form method="post">
-		<div style='width: 50%; margin-left: auto; margin-right: auto'>
+			#anotationArea { 
+	 	 		width:80%;
+				padding:40px 0px 0px 150px; 
+	 	 		margin-left: auto;
+				margin-right: auto;
+			}
+			#citationArea { 
+	 	 		width:80%;
+				padding:20px 0px 0px 150px; 
+	 	 		margin-left: auto;
+				margin-right: auto;
+			}
+			#tagArea { 
+	 	 		width:80%;
+				padding:20px 0px 0px 0px; 
+	 	 		margin-left: auto;
+				margin-right: auto;
+			}
+			#submitArea { 
+	 	 		width:80%;
+				padding:20px 0px 0px 0px; 
+	 	 		margin-left: auto;
+				margin-right: auto;
+				text-align: right;
+			}
+			#title { 
+	 	 		width:60%;
+				padding:0px; 
+	 	 		margin-left: 0%;
+				margin-right: auto;
+				float: left;
+			}
+			#select { 
+	 	 		margin-right: 14%;
+				float: right;
+			}
+			
+			.title{
+				float: left;
+				width: 35%;
+				text-align: left;
+				font-family:Ariel, Verdana, Sans-serif;
+				font-style:italic;
+				font-size:17px;
+				color:#0300ee;
+			}
+			
+			input.myButton { 
+			   font-family: Arial, Helvetica, sans-serif; 
+			   font-size: 30px; 
+			   background-color: #bcffaf;
+			   margin-top: 2%;
+			   margin-left: 10%; 
+			   padding: 2px; 
+			   height: 50px; 
+			   width: 800px; 
+			   border: 1px solid #000000;
+			   -moz-border-radius-topleft: 50px; 
+			   -moz-border-radius-topright: 50px; 
+			   -moz-border-radius-bottomleft: 50px; 
+			   -moz-border-radius-bottomright: 50px; 
+			} 
+			
+	    </style>
 		
-			Title 
-			<div style='width: 70%'>
-				<input type="text" id="eloName" name="eloName" value="<?= $citation->get_title() ?>" style='width: 70%'/>
-			</div>
-			
-			
-			<div style='margin-top: 20px; margin-bottom: 20px; width: 90%;'>
-				<label for='eloContent'>Description</label>
+		<script type='text/javascript'>
+
+			// disable the form submission, if the user accidentaly
+			// hits the enter button, while the cursor is in a textbox. 
+			// There is no need to do this for TextAreas
+			function disableEnterKey(e)
+			{
+			     var key;      
+			     if(window.event)
+			          key = window.event.keyCode; //IE
+			     else
+			          key = e.which; //firefox      
+			     return (key != 13);
+			}
+
+		</script>
+	</head>
+	
+	
+	<body>
+		<form method="post" action="referencePage.php">
+				<div id='titleArea'>
+					<div id='title'>
+						<font size='4'>Reference Title</font>
+						<input type="text" id="referenceTitle" name="referenceTitle" onKeyPress="return disableEnterKey(event)" style="width:300px" value="<?= $reference->get_title() ?>"/>
+					</div>
+					<div id='select'>
+						<select id="referenceCategoryList" name="referenceCategoryList"> 
+							<?php 
+								$referenceCategories = new ReferenceCategories();
+								$refCatsList = $referenceCategories->getReferenceCategories(); 
+								
+								// Fill in the reference's category
+								if (isset($_REQUEST['id'])){
+									$result = "<option value=''>" . $reference->get_category() . "</option>";
+								}else {
+									$result = "<option value=''> Select a Category </option>";									
+								}
+								for ($i = 0; $i < sizeof($refCatsList); $i++){
+									$result .= "<option value='".$i."'>".$refCatsList[$i]."</option>";
+								}
+								echo $result;
+							?>
+						</select>
+					</div>
+		  		</div>
+		  		
+				<div id='anotationArea' >
+					<font size='4'>Annotation</font><br/>
+					<textarea id="annotationText" name="annotationText" rows='10' cols='75' id='anotation'> <?= $reference->get_annotation() ?></textarea>
+				</div>
+				
+				<div id='citationArea'>
+					<font size='4'>Citation</font> <br/>
+					<textarea id="citationText" name="citationText" rows='10' cols='75' id='citation'> <?= $reference->get_citation() ?> </textarea>
+				</div>
+
+				<div id='tagArea' >
+					<div id='tag'>
+						<font size='4'>Tags</font>
+						<textarea id="tags" name="tags" style="width:800px; height: 100px;"><?= $refernceTagsString?> </textarea>
+					</div>
+				</div>
+				
+				<div style='width: 100%; height: 20%; float:left'>
+					<input type="hidden" id="action" name="action" value="<?= $action?>" />
+					<input type="hidden" id="id" name="id" value="<?= $reference->get_id() ?>" />
+					<input type="submit" value='Submit' class='myButton'>
+				</div>
+				
 				<br/>
-				<textarea id="eloContent" name="eloContent" class="tinymce" style='float: left'>
-					<?= htmlentities($citation->getContent()) ?>
-				</textarea>
-			</div>
+				<br/>
+				<br/>
+				<br/>
+	 	</form>	
 	
-			<!-- Some integration calls -->
-
-
-			<div style='margin-top: 20px;'>
-				<label for='eloTags'>Tags</label>
-				<input id='eloTags' type="text" size="100%" value='<?= $citationTagsString?>' style='margin-top: 10px; margin-bottom: 10px; width 100%; float: left'>
-			</div>		
 	
-			<div style='margin-top:1px; float: right'>
-				<input type="submit" name="save" value="Submit"/>
-				<input type="reset" name="reset" value="Reset"/>
-			</div>
-			
-			
-		</div>
-		
-		<input type="hidden" id="action" name="action" value="<?= $action?>" />
-		<input type="hidden" id="eloId" name="eloId" value="<?=$_REQUEST['id'] ?>" />
-		<input type="hidden" id="eloVersion" name="eloVersion" value="<?=$citation->get_version() ?>" />
-	</form>
-</body>
-
-</html>
+<?php 
+	require_once 'footer.php';
+?>
+	
