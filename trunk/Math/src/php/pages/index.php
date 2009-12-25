419b08f9d8ac1aa5dc4dde55fb8f50e210d391a2
somebodyiknow <somebodyiknow@294c3b0c-171f-11de-a6cc-771c510851eb>
2009-12-25 15:39:28 +0000

    Montreal Demo version

--- trunk/Math/src/php/pages/index.php
+++ trunk/Math/src/php/pages/index.php
@@ -1,9 +1,15 @@
 <?php 
 require_once './header.php';
+require_once '../graphML/GraphML.php';
 
 if (isset($_REQUEST['username'])){
 	$_SESSION['username'] = $_REQUEST['username'];	
 }
+
+$graphML = new GraphML();
+$groups = array('Geometry', 'Trigonametry', 'Exponential', 'Algebra');
+$graphML->setGroups($groups);
+$graphML->createGroupGraphMLs();
 
 ?>
  
@@ -25,7 +31,7 @@ if (isset($_REQUEST['username'])){
 
 <ol>
 	<li> Tag questions <br/>
-	     <input type="button" class="SmallButton" value="Tag Questions > " onClick="document.location.href='tag_question.php';"/>
+	     <input type="button" class="SmallButton" value="Tag Questions > " onClick="document.location.href='/src/php/pages/tag_question.php';"/>
 	</li>
 </ol>
 
