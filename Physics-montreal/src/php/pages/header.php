a42d7dfbdf8789b42b5555f49213453287a24556
somebodyiknow@gmail.com <somebodyiknow@gmail.com@294c3b0c-171f-11de-a6cc-771c510851eb>
2010-03-08 18:28:28 +0000

    logout button

--- Physics-montreal/src/php/pages/header.php
+++ Physics-montreal/src/php/pages/header.php
@@ -14,7 +14,8 @@ session_start();
 	<body>
 		<div style='margin-top: 4%; margin-left:20%'>
 			<font size='5' color='blue'> UTS M3 Science </font><br/>
-			<font size='9' onClick="document.location.href='/src/php/pages/tag_question.php?username=<?= $_SESSION['username']?>'" style='cursor:pointer;'> Physics </font> 
+			<font size='9' onClick="document.location.href='/src/php/pages/index.php'" style='cursor:pointer;'> Physics </font> 
+			<font size='3' onClick="document.location.href='/src/php/ajaxServices/logout.php'" style='cursor:pointer; color:blue; margin-left:55%'> Sign Out </font> 
 		</div>
 
 		<div id='main' style='height: 70%'> 
