ab321c4a3bf4c5a6c110d9f2b793d9625c35e8d7
somebodyiknow@gmail.com <somebodyiknow@gmail.com@294c3b0c-171f-11de-a6cc-771c510851eb>
2010-09-03 13:58:13 +0000

    Add Comment

--- trunk/Physics-montreal-fall-2010/src/php/dataModels/RunConfig.php
+++ trunk/Physics-montreal-fall-2010/src/php/dataModels/RunConfig.php
@@ -7,7 +7,15 @@ class RunConfig extends Elo {
 		parent::__construct($xml);
 		parent::addMetadata('type', 'RunConfig');
 	}
-	
+	/*
+	 *	The variable $name for getter & seter 
+	 *
+	 *  $name="runVersion"
+	 *	$name="runClass" 
+	 *	$name="runId" 
+	 *	$name="runChoiceLimit" 
+	 *	$name="runPublished" 
+	 */
 	public function __set($name, $value) {
 		$name = strtolower($name);
 		parent::addMetadata($name, $value);
