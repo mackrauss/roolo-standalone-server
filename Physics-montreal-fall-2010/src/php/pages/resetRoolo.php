99e9795ea537fe098511927a5946b44c2d7ef83b
somebodyiknow@gmail.com <somebodyiknow@gmail.com@294c3b0c-171f-11de-a6cc-771c510851eb>
2010-09-03 13:56:27 +0000

    add create runConfig object

--- Physics-montreal-fall-2010/src/php/pages/resetRoolo.php
+++ Physics-montreal-fall-2010/src/php/pages/resetRoolo.php
@@ -1,7 +1,7 @@
 <?php
 include_once '../RooloClient.php';
 include_once '../dataModels/TeacherProgress.php';
-
+include_once '../dataModels/RunConfig.php';
 $roolo = new RooloClient();
 
 $roolo->deleteAll();
@@ -20,7 +20,18 @@ $tp = new TeacherProgress();
 $tp->set_progress('');
 $savedTpXml = $roolo->addElo($tp);
 
-echo $savedTpXml;
+//echo $savedTpXml;
+
+/*
+ * Create RunConfig
+ */
+$rc = new RunConfig();
+$rc->runid = 'v1-c1';
+$rc->runchoicelimit = "A, B, C, D";
+$savedRcXml = $roolo->addElo($rc);
+
+echo $savedRcXml;
+
 ?>
 
 <script type='text/javascript'>
